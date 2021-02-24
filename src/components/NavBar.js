3c2cc861cae8c6e5691dfd5548fff26196fb33e5
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-02-24 10:54:55 +0000

    navbar media queries added. Display isnt quite working and colours added for checking purposes only

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,28 @@
-import '../styles/navbar.css'
+import '../styles/navbar.css';
+import { useState } from 'react'
+
 
 function NavBar() {
+
+    const[showLinks, setShowLinks] = useState(false);
+
+
+
     return (
         <div className="navbar">
-            <div className="nav-left"></div>
+            <div className="nav-left">
+            <div className="cpm-logo">
+                <a>INSERT CPM IMAGE HERE</a>
+            </div>
+            </div>
             
             <div className="nav-right">
-                <div className='links'>
+                <div className='links' id={showLinks ? "hidden" : "" }>
                     <a href="/">Home</a>
                     <a href="/about">About</a>
                     <a href="/contact">Contact</a>
-
                 </div>
+                <button onClick={() => setShowLinks(!showLinks)}  >Open</button>
             </div>
 
 
