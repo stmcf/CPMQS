c828cbbdf1a92b1e9d293d27758bd58fccdd1d50
stmcf <stevenmcfarlane@hotmail.co.uk>
2021-02-25 12:22:05 +0000

    grid layout added to home.js and started to style with background image and copy.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,47 @@
-
+import '../styles/home.css'
 
 
 
 function Home() {
     return (
-        <div>
-           
+        <div class="grid-container">
+            <div class="who-why">
+                <div class="who-why-title">
+                    <article>
+                        <p>
+                            CPM Quantity Surveying is an SME (Sub)Contractor focused consultancy offering commercial management services on a bespoke and ad-hoc basis.  Having spent a combined 30 years plus experience in commercial roles within the construction industry our management team realised that smaller contractors were under represented on commercial and contractual matters and were compelled to provide a solution. 
+                        </p>
+                        <p>  
+                            We have decided to do things differently and, through a schedule of rates, agreed in advance, we offer the right level of consultant for each task essentially providing a full, on-demand commercial department from Junior QS’s and Commercial Assistants through to Commercial Managers.  This model ensure your business benefits from the knowledge, experience and supervision of our senior consultants without having to pay for them to carry out the day to day commercial tasks.     
+
+                        </p>
+                        
+                    </article>
+                </div>
+            </div>
+                <div class="who-why-blurb">
+
+                </div>
             
+            
+            
+            <div class="image">
+                <img src="'../images/james-sullivan-ESZRBtkQ_f8-unsplash.jpg" alt="" srcset=""/>
+            </div>
+
+
+
+            <div class="news-container">
+                <div class="news-contactus"></div>
+                <div class="news-li-feed"></div>
+            </div>
+            <div class="values-container"></div>
+
+
+
         </div>
+
+    
     );
 };
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -2,10 +2,7 @@ function Services() {
     return (
         <div>
             <div className='service-blurb'>
-                CPM Quantity Surveying is an SME (Sub)Contractor focused consultancy offering commercial management services on a bespoke and ad-hoc basis.  Having spent a combined 30 years plus experience in commercial roles within the construction industry our management team realised that smaller contractors were under represented on commercial and contractual matters and were compelled to provide a solution. 
-           
-                We have decided to do things differently and, through a schedule of rates, agreed in advance, we offer the right level of consultant for each task essentially providing a full, on-demand commercial department from Junior QS’s and Commercial Assistants through to Commercial Managers.  This model ensure your business benefits from the knowledge, experience and supervision of our senior consultants without having to pay for them to carry out the day to day commercial tasks.     
-                </div>
+                
 
                 <div>
             Here at CPM we don’t believe you should have to pay for experience or time that you don’t need and as such we do things slightly differently.  With us you pay for hours you need and the QS you need for each task giving you access to an entire commercial department as and when you need it.  We agree a schedule of rates in advance for all levels of commercial support that you can call upon when you need it to ensure your business is commercial and contractually protected while driving efficiency and enhanced margins. 
@@ -40,6 +37,7 @@ function Services() {
                 Preparation of Final Account 
                 Assessment of Subcontractors Final Account submissions 
                 Negotiation and agreement of Final Accounts 
+                </div>
         </div>
         </div>
     )
